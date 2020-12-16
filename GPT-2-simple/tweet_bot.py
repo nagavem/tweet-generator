cc554bd1ef7bf7ce576532d51cfff2d3c3b2c341
poojansheth <63199421+poojansheth@users.noreply.github.com>
2020-12-16 23:16:28 +0000

    Update tweet_bot.py

--- GPT-2-simple/tweet_bot.py
+++ GPT-2-simple/tweet_bot.py
@@ -4,10 +4,10 @@ import generate_tweets as gt
 mode='print'  #has to be 'tweet' or 'print'
 
 if mode=='tweet':
-	consumer_key = 'dJ30fM3EKMumCePiyJNJ3e7ob' 
-	consumer_secret = 'u5XNXRxduS3WHD2JzZIgyvDxsevYD6818CMdMEc5Z0v3reQqun' 
-	access_token = '1338787696412651522-IW0b60Itfgyk7T52lkGvmdDOqT4sMQ' 
-	access_token_secret = 'Z72CYDo9EfuzEojnanTDDHeURb6VQ3poBjoo7rSwK15Mb' 
+	consumer_key = '' 
+	consumer_secret = '' 
+	access_token = '' 
+	access_token_secret = '' 
 
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
 	auth.set_access_token(access_token, access_token_secret)
